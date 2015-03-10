1acd691bf3ac9abacda6b0781d2acced4f34873d
gerard deschamps <gerard.deschamps@atos.net>
2015-03-10 13:31:30 +0100

    ajout des tests protractor lancement via console lancement via grunt

--- adf-web/Gruntfile.js
+++ adf-web/Gruntfile.js
@@ -2,17 +2,34 @@ module.exports = function(grunt) {
 
 	// Project configuration.
 	grunt.initConfig({
-		karma: {
-			  unit: {
-			    configFile: 'myKarma.conf.js',
-			    singleRun: true,
-			  },
-			}
+		karma : {
+			unit : {
+				configFile : 'myKarma.conf.js',
+				singleRun : true,
+			},
+		},
+
+		protractor_webdriver : {
+			options : {},
+			your_target : {},
+		},
+
+		protractor : {
+			options : {},
+			your_target : {
+				options : {
+					configFile : "protractor.conf.js",
+				}
+			},
+		}
+
 	});
 
 	grunt.loadNpmTasks('grunt-karma');
+	grunt.loadNpmTasks('grunt-protractor-webdriver');
+	grunt.loadNpmTasks('grunt-protractor-runner');
 
 	// Default task(s).
-	grunt.registerTask('tests', [ 'karma']);
+	grunt.registerTask('tests', [ 'karma' , 'protractor_webdriver', 'protractor' ]);
 
 };
